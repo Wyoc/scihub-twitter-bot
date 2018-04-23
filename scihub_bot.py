4f49b72bc16c7c6aa8329d821979218419975378
wyoc <dirtykartoons@gmail.com>
2018-04-23 15:55:49 +0200

    feat(twitter): add automatic tweet

--- scihub_bot.py
+++ scihub_bot.py
@@ -3,6 +3,8 @@ import re
 import urllib.request
 import socket
 from pprint import pprint
+import configparser
+import twitter
 
 
 def check_url(url, timeout=5 ):
@@ -30,6 +32,33 @@ def get_scihub_mirrors_up():
 
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    consumer_key = config['twitter_connect']['consumer_key']
+    consumer_secret = config['twitter_connect']['consumer_secret']
+    access_token = config['twitter_connect']['access_token']
+    access_token_secret = config['twitter_connect']['access_token_secret']
+
+    print(consumer_key)
+    print(consumer_secret)
+    print(access_token)
+    print(access_token_secret)
+
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_token_secret)
 
     urls = get_scihub_mirrors_up()
     pprint(urls)
+
+    message = '🚀 Sci-hub mirrors aviable today:'
+    for url in urls:
+        message = message+"\n- {}".format(url)
+
+    print(message)
+    try:
+        status = api.PostUpdate(message)
+    except UnicodeDecodeError:
+        print("Your message could not be encoded.  Perhaps it contains non-ASCII characters? ")
+        print("Try explicitly specifying the encoding with the --encoding flag")
